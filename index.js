6f0dcc71446d42173013bd46bf901e1f892f8079
nasafato <alanjgou@gmail.com>
2019-06-17 21:13:31 -0700

    Second pass at spec-compliant Promise
    The first attempt was just a bluthering rush into implementing a
    Promise. It meant I had to figure out the API and all its edge
    cases and rules, *and* implement it, *and* figure out tests.
    
    This second pass is more to exercise the second part - implementing
    the spec - instead of doing all three parts.

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ var states = {
   REJECTED: 3,
 }
 
-function createThen(callbacks, onFulfilled, onRejected) {
-  let newResolve, newReject
-  const newPromise = new MyPromise((resolve, reject) => {
-    newResolve = resolve
-    newReject = reject
-  })
-
-  callbacks.push({
-    onFulfilled(value) {
-      onFulfilled(value)
-      newResolve(value)
-    },
-    onRejected(reason) {
-      onRejected(reason)
-      newReject(reason)
-    },
-  })
-  return newPromise
-}
-
 function MyPromise(executor) {
   let state = states.PENDING
   let value = undefined
@@ -35,12 +15,16 @@ function MyPromise(executor) {
 
     state = states.FULFILLED
     value = value
+    // then.6 - when a promise is fulfilled or rejected, execute onFulfilled callbacks
+    // or onRejected callbacks in order of calls
     while (callbacks.length > 0) {
       const callback = callbacks.pop()
+      const { onFulfilled, onRejected } = callback
       try {
-        callback.onFulfilled(value)
+        // then.5 must be called as functions
+        onFulfilled(value)
       } catch (e) {
-        callback.onRejected(e)
+        onRejected(e)
       }
     }
   }
@@ -48,7 +32,25 @@ function MyPromise(executor) {
   executor(resolve, reject)
 
   function then(onFulfilled, onRejected) {
-    return createThen(callbacks, onFulfilled, onRejected)
+    // then.6 if promise is fulfilled/rejected, execute callbacks
+    const [callbackObj, newPromise] = createThen(
+      onFulfilled,
+      onRejected,
+      state,
+      callbacks
+    )
+    if (state === states.FULFILLED) {
+      callbackObj.onFulfilled.call(undefined, value)
+    } else if (state === states.REJECTED) {
+      // then.5 must be called as functions
+      callbackObj.onRejected.call(undefined, reason)
+    } else {
+      // then.5 must be called as functions
+      callbacks.push(callbackObj)
+    }
+
+    // then.7 must return a promise
+    return newPromise
   }
 
   return {
@@ -56,6 +58,86 @@ function MyPromise(executor) {
     value,
     reason,
     then,
+  }
+}
+
+function createThen(onFulfilled, onRejected) {
+  let newResolve, newReject
+  const newPromise = new MyPromise((resolve, reject) => {
+    newResolve = resolve
+    newReject = reject
+  })
+
+  const callbackObj = {
+    onFulfilled: function wrappedOnFulfilled(value) {
+      // then.1 both onFulfilled and onRejected are optional arguments
+      if (typeof onFulfilled !== 'function') {
+        // then.7.ii if onFulfilled is not a function and promise is fulfilled, resolve
+        // with value
+        newResolve(value)
+        return
+      }
+      try {
+        const value = onFulfilled(value)
+        doResolve(newResolve, newReject, value)
+      } catch (e) {
+        newReject(e)
+      }
+    },
+    onRejected: function wrappedOnRejected(reason) {
+      // then.1 both onFulfilled and onRejected are optional arguments
+      if (typeof onRejected !== 'function') {
+        // then.7.iii if onRejected is not a function and promise is rejected, reject
+        // with reason
+        newReject(reason)
+        return
+      }
+      try {
+        const value = onRejected(reason)
+        doResolve(newResolve, newReject, value)
+      } catch (e) {
+        newReject(e)
+      }
+    },
+  }
+
+  return [callbackObj, newPromise]
+}
+
+function doResolve(resolve, reject, value) {
+  if (value instanceof MyPromise) {
+    value.then(val => resolve(val), reason => reject(reason))
+    return
+  }
+
+  if (typeof value !== 'object' && typeof value !== 'function') {
+    resolve(value)
+    return
+  }
+
+  let resolvePromiseCalled = false
+  let rejectPromiseCalled = false
+
+  try {
+    const then = value.then
+    if (typeof then !== 'function') {
+      resolve(value)
+      return
+    }
+    function resolvePromise(val) {
+      if (resolvePromiseCalled || rejectPromiseCalled) return
+      resolvePromiseCalled = true
+      doResolve(resolve, reject, val)
+    }
+    function rejectPromise(r) {
+      if (resolvePromiseCalled || rejectPromiseCalled) return
+      rejectPromiseCalled = true
+      reject(r)
+    }
+    then.call(value, resolvePromise, rejectPromise)
+  } catch (e) {
+    if (resolvePromiseCalled || rejectPromiseCalled) return
+    reject(e)
   }
 }
 
